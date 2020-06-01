46d09c6ead179ea549066968c923ce290698ad36
andrewMacmurray <a.macmurray@icloud.com>
2020-06-01 19:40:02 +0100

    make combining arguments a bit less ugly

--- frontend/src/elm/Api/Argument.elm
+++ frontend/src/elm/Api/Argument.elm
@@ -1,13 +1,69 @@
 module Api.Argument exposing
-    ( created_at
+    ( combine2
+    , combine4
+    , created_at
     , eq_
+    , field
     , order_by
+    , prop
     , tag
     , tags
     , where_
     )
 
 import Graphql.OptionalArgument exposing (OptionalArgument(..))
+
+
+
+-- Helpers for combining Nested Graphql Argument Queries
+
+
+type alias Field a b =
+    { field : a
+    , expression : b
+    }
+
+
+type alias Prop a b =
+    { field : a
+    , value : b
+    }
+
+
+
+-- Combine
+
+
+combine2 : Field (input -> output) (c -> input) -> Prop (d -> c) d -> output
+combine2 a b =
+    a.field (a.expression (b.field b.value))
+
+
+combine4 : Field (a -> b) (c -> a) -> Field (d -> c) (e -> d) -> Field (f -> e) (j -> f) -> Prop (k -> j) k -> b
+combine4 a b c d =
+    a.field (a.expression (b.field (b.expression (c.field (c.expression (d.field d.value))))))
+
+
+
+-- Field
+
+
+field : field -> expr -> Field field expr
+field a b =
+    { field = a
+    , expression = b
+    }
+
+
+
+-- Prop
+
+
+prop : field -> value -> Prop field value
+prop a b =
+    { field = a
+    , value = b
+    }
 
 
 
