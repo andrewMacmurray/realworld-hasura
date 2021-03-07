acc427d632d6480ef7bd29480aa7fd76b71642d1
andrewMacmurray <a.macmurray@icloud.com>
2021-03-07 13:46:52 +0000

    removes test.login

--- backend/actions/test/Main.purs
+++ backend/actions/test/Main.purs
@@ -2,11 +2,8 @@ module Test.Main where
 
 import Prelude
 import Effect (Effect)
-import Test.Login as Login
 import Test.Password as Password
 import Test.Unit.Main (runTest)
 
 main :: Effect Unit
-main =
-  runTest do
-    Password.suite
+main = runTest do Password.suite
